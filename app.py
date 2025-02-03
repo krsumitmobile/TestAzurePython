83d3c5b8a6dd5eabe0f7f06a7242d29d9478a3c2
krsumitmobile <krsumit@gmail.com>
2025-02-03 15:59:38 +0530

    second checkin

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, World! Flask App Deployed on Azure with CI/CD By Sumit🚀"
+    return "Hello, World! Flask App Deployed on Azure with CI/CD By Sumit. This is second cimit🚀"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
