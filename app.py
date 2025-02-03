c547bfefff44df9371deb7fc0f86ad3ac0ec5f30
krsumitmobile <krsumit@gmail.com>
2025-02-03 15:43:17 +0530

    Initial checkin

---
+++ app.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  3 15:33:29 2025
+
+@author: User
+"""
+
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  3 13:00:20 2025
+
+@author: saurabh.agarwal
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello, World! Flask App Deployed on Azure with CI/CD By Sumit🚀"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
---
+++ startup.sh
@@ -0,0 +1,1 @@
+#!/bin/bashgunicorn -b 0.0.0.0:8000 app:app
